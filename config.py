78b1fca5b30d99fd5e5158a00b724bf201dcdc7d
oddlyspaced <oddlyspaced1@gmail.com>
2019-07-06 12:06:58 +0530

    Update config

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config :
+    system_img = ""
+    vendor_img = ""
+    boot_img = ""
+    rom_base_git = "https://github.com/oddlyspaced/oos-violet-rom-base.git"
+    device_codename = "violet"
